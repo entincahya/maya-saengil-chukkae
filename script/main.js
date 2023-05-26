cc596e4937e8c98d0ae632275ef37c7de06701e7
Entin <entincahya96@gmail.com>
2023-05-27 00:42:05 +0700

    hbd

--- script/main.js
+++ script/main.js
@@ -28,15 +28,13 @@ const fetchData = () => {
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
   const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
-  const hbd = document.getElementsByClassName("wish-hbd")[0];
+  
 
   textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
     .split("")
     .join("</span><span>")}</span`;
 
-  hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
+  
 
   const ideaTextTrans = {
     opacity: 0,
